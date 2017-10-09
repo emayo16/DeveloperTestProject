179a753566a7eee6c02261beb1ac7d2d64c1b759
emayo16 <emayo16@gmail.com>
2017-10-09 08:51:51 -0400

    Fixed issue with database and table creation on first start

--- app/database/db.js
+++ app/database/db.js
@@ -6,16 +6,19 @@ var dbconfig = {host: "localhost", port: 28015, db: "testProject"};
 r.connect( dbconfig, function(err, conn) {
 	if (err) throw err;
     // Check to see that 'tracks' table is created with index 'createdAt'
-    r.table("tracks").indexWait("createdAt").run(conn).then(function(err, result) {
+    r.db(dbconfig.db).table("tracks").indexWait("createdAt").run(conn).then(function(err, result) {
         console.log("tracks table is created.");
     }).error(function(err) {
         // The database/table/index do not exist. Create them.
+        console.log("before dbcreate: " + err);
         r.dbCreate(dbconfig.db).run(conn).finally(function() {
-            return r.tableCreate("tracks").run(conn)
+            console.log("before tableCreate");
+            if(r.db(dbconfig.db).tableList().contains("tracks")){
+                console.log("inside cotnains");
+                return r.db(dbconfig.db).tableCreate("tracks").run(conn)
+            }
         }).finally(function() {
-            r.table("tracks").indexCreate("createdAt").run(conn);
-        }).finally(function(result) {
-            r.table("tracks").indexWait("createdAt").run(conn)
+                r.db(dbconfig.db).table("tracks").indexCreate("createdAt").run(conn); 
         }).then(function(result) {
             console.log("'tracks' table was created with index 'createdAt'");
         }).error(function(err) {
@@ -23,9 +26,10 @@ r.connect( dbconfig, function(err, conn) {
                 console.log(err);
                 process.exit(1);
             }
-            console.log("'tracks' table was created with index 'createdAt'");
+            //console.log("'tracks' table was created with index 'createdAt'");
         });
     });
+    var insertTestData = process.argv[2];
     var testTracks = [{ "createdAt": r.now(), "name": "Carnival Ecstacy", 
     "latitude": "32.78083", "longitude": "-79.9234", "callsign": "H3GR", 
     "mmsid": "353479000", "speed": "10.6", "course": "90", "heading": "90"}, 
@@ -75,8 +79,12 @@ r.connect( dbconfig, function(err, conn) {
     "latitude": "30.5899696", "longitude": "-117.440498", "callsign": "YJWD2", 
     "mmsid": "577357000", "speed": "7.5", "course": "197", "heading": "180"}];
     // Insert Test Track Data in DB
-    r.table('tracks').insert(testTracks).run(conn, function(err, result) {
-        if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-    });
+    if(insertTestData === "insertData"){
+        r.table("tracks").insert(testTracks).run(conn, function(err, result) {
+            if (err) throw err;
+            console.log(JSON.stringify(result, null, 2));
+        }).error(function(err){
+            console.log("error inserting test tracks");
+        });  
+    }
 });
